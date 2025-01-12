89aa1e09b85856f84878dc60eb2176883c49ed7d
James Moses <jamxsmosxs@gmail.com>
2025-01-12 15:14:43 +0100

    Resolve build path in vite config.js

--- src/pages/Search/movie information/MovieInformation.jsx
+++ src/pages/Search/movie information/MovieInformation.jsx
@@ -1,5 +1,5 @@
 import "./MovieInformation.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 
 function MovieInformation() {
@@ -7,6 +7,8 @@ function MovieInformation() {
   const [data2, setData2] = useState();
 
   const [status2, setStatus2] = useState(false);
+
+  const ref = useRef("");
 
   console.log(status2);
 
@@ -54,14 +56,17 @@ function MovieInformation() {
       const newFavorites = favorites.filter((movie) => movie.imdbID !== id);
 
       localStorage.setItem("favorites", JSON.stringify(newFavorites));
+      // checkMovieExist();
+
+      ref.current = "cls-1-white";
     } else {
       // if it doesn't, push object to array
       favorites.push(data2);
       // Add array to localStorage
       localStorage.setItem("favorites", JSON.stringify(favorites));
+      ref.current = "cls-1";
+      // checkMovieExist();
     }
-
-    checkMovieExist();
   }
 
   const length = data2.Ratings.length;
@@ -70,14 +75,12 @@ function MovieInformation() {
   // Retrieve favorite movies array from localStorage
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-  let movieExist;
-
   // check if current movie exists in local storage
   function checkMovieExist() {
     if (favorites.some((movie) => movie.imdbID === id)) {
-      movieExist = true;
+      ref.current = "cls-1";
     } else {
-      movieExist = false;
+      ref.current = "cls-1-white";
     }
   }
 
@@ -98,10 +101,7 @@ function MovieInformation() {
                 <g id="Layer_2" data-name="Layer 2">
                   <g id="Layer_1-2" data-name="Layer 1">
                     <path
-                      style={{
-                        fill: `${movieExist === true ? "orangered" : "white"}`,
-                      }}
-                      className="cls-1"
+                      className={ref.current}
                       d="M65.89,0c23.13.62,41.64,12.1,55.4,31.85,2.32,3.33,3.2,3.81,5.76.11,12.44-18,29-29.64,51.29-31.56C201.85-1.64,220,8.55,234,26.54c19.27,24.69,17.4,51.26,5.39,78.13C231.32,122.73,218,137.1,204.33,151c-25.13,25.48-52,49.11-77.9,73.8-1.31,1.26-2.25,2.2-4.05.45-31.94-31.07-66.61-59.38-95.66-93.39C12.87,115.63,3.54,97.33.57,75.71-4.88,36,29.45-.29,65.89,0Z"
                     />
                   </g>
@@ -136,7 +136,7 @@ function MovieInformation() {
                     {length > 1 ? (
                       data2.Ratings.map((rating) => (
                         <div key={data2.Ratings.indexOf(rating)}>
-                          <p>{`${rating.Source}: `}</p>
+                          <p className="source">{`${rating.Source}: `}</p>
                           <span>{rating.Value}</span>
                         </div>
                       ))
